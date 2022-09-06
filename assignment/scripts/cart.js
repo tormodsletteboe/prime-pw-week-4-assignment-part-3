c3b1f126bb60d3a2ff63531972fa7b8d6d20a07c
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-06 17:28:00 -0500

    did parts through stretch 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,106 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+const maxItems =5;
+
+
+function addItem(item){
+        if(isFull()==false){
+            basket.push(item);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+   
+
+
+function listItems(){
+    let lengthOfBasket = basket.length;
+    for(let i =0; i<lengthOfBasket;i++){
+        console.log(basket[i]);
+    }
+
+    //tried this first, but it just returned 0.
+    // for(item in basket){
+    //     console.log(item);
+    // }
+}
+
+function empty(){
+    basket=[];
+}
+
+function runAfterTest(){
+    console.log('basket item count: '+basket.length);
+    console.log('Basket is now: '+ basket);
+    console.log('----')
+}
+//----------------------------stretch------------------
+function isFull(){
+    if(basket.length<maxItems){
+        return false;  
+    }
+    else{
+        return true;
+    }
+}
+
+
+//---------------------------------------------------tests-------------------------
+//test adding 1 item
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+runAfterTest();
+
+//test adding empty string
+console.log('test: add empty string \'\'');
+console.log('Adding \'\' (expect true)', addItem(''));
+runAfterTest();
+
+//test call additem without arguments
+console.log('test: add missing arg');
+console.log('Adding item with no argument, (expect true)', addItem());
+runAfterTest();
+
+//list items in basket
+console.log(`Basket is ${basket}`);
+console.log('listing items in basket with a loop');
+listItems();
+runAfterTest();
+
+//testing listitems call on empty basket
+console.log('empty out basket, then call listItems');
+empty();
+listItems();
+runAfterTest();
+
+//test listitems with more than 1 items in basket
+console.log('adding 4 items');
+console.log(`Basket is ${basket}`);
+for(let i =0; i<5;i++){
+    addItem('item number'+i);
+}
+listItems();
+runAfterTest();
+
+
+//redundant test but for clarifications, test to make sure basket becomes empty
+console.log('calling empty, basket should be empty')
+empty();
+console.log(`basket is ${basket}`);
+runAfterTest();
+
+//-------------------stretch tests-----------------------
+
+//test to add item 1 greater than max
+console.log('calling empty first');
+empty();
+console.log('trying to add 10 items, (should say true 5 times, then false 5 times');
+for(let i =0; i<10;i++){
+   console.log( addItem('item number: '+i));
+}
+runAfterTest();
